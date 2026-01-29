eaf19ce24d9def3f0ba4f9cf5662955523273add
Allen Francis <lallenfrancisl@gmail.com>
2026-01-29 23:06:41 +0530

    feat: define tokens of monkey lang

---
+++ internal/token/token.go
@@ -0,0 +1,34 @@
+// Package token contains all the tokens used by monkey language
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identfiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
