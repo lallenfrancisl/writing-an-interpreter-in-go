15524dbbdd387bb6fcc563cad750cfca329ca936
Allen Francis <lallenfrancisl@gmail.com>
2026-06-03 03:49:12 +0530

    feat: add support for return statements

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -15,7 +15,7 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -37,8 +37,13 @@ func Eval(node ast.Node) object.Object {
 
 		return evalInfixExpression(left, node.Operator, right)
 
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+
+		return &object.ReturnValue{Value: val}
+
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 
 	case *ast.IfExpression:
 		return evalIfExpression(node)
@@ -48,11 +53,29 @@ func Eval(node ast.Node) object.Object {
 	}
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(stmts []ast.Statement) object.Object {
 	var result object.Object
 
 	for _, stmt := range stmts {
 		result = Eval(stmt)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
 	}
 
 	return result
